a30691d4eecfeb9e02fb84e7eccba2c92964af91
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2019-11-29 23:49:29 -0600

    Set Process "DD_Freight_Process" in Column DocAction of table DD_Freight Order.
    Otherwise, it is not possible to call prepare() or complete().

---
+++ migration/containerZeca/21_Set_WFProcess_for_FreightOrder.sql
@@ -0,0 +1,4 @@
+-- Set Process "DD_Freight_Process" in Column DocAction of table DD_Freight Order
+UPDATE AD_Column SET AD_Process_ID=53954,Updated=TO_TIMESTAMP('2019-11-29 23:39:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=83740
+;
+
