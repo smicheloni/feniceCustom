02f00c285ad0b5281b2ca55218950271ac660371
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2019-12-07 10:46:00 -0600

    Correct parameter M_Inout_ID of Process "SB_InoutCreateFromFreight" to be Table+M_InOut_Order

---
+++ migration/containerZeca/25_Parameter_on_MInOut.sql
@@ -0,0 +1,5 @@
+﻿-- Set M_Inout:ID parameter to be Table+M_InOut_Order on Process "SB_InoutCreateFromFreight"
+-- Make sure the Parameter's ID = 1001784  !!
+UPDATE AD_Process_Para SET AD_Reference_ID=18, AD_Reference_Value_ID=337, AD_Val_Rule_ID=233, ReadOnlyLogic='1=1',Updated=TO_TIMESTAMP('2019-12-07 09:56:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1001784
+;
+
